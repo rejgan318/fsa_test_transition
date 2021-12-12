61e7baa82fe9761db3b957fe0ed7a8ca118b0b6d
rejgan318 <49007125+rejgan318@users.noreply.github.com>
2021-12-12 18:18:54 +0200

    Обработка входа и выхода в состояние

--- fsa/ABC_state_machine.py
+++ fsa/ABC_state_machine.py
@@ -1,18 +1,20 @@
 """ Простейший конечный автомат """
-from transitions import Machine
+from transitions import Machine, State
 from enum import Enum, auto
 import logging
 
 class AbcClass(object):
     def __init__(self):
-        self.symbol = ''
+        self.symbol = 'а'
 
     def input(self):
+        print('inputting....')
         self.symbol = input('Один символ и Enter:')
         self.symbol = self.symbol[0]
 
     def output(self):
-        print(f'{self.symbol=}')
+        print(f'символ {self.symbol=}')
+
 
 class States(Enum):
     INPUT = auto()
@@ -21,10 +23,17 @@ class States(Enum):
 
 
 if __name__ == '__main__':
+
     logging.basicConfig(level=logging.DEBUG)
     abc_o = AbcClass()
-    abc_m = Machine(model=abc_o, states=States)
+    abc_m = Machine(model=abc_o, states=States, initial=States.INPUT)
     abc_m.add_transition('ip', States.INPUT, States.PROCESS)
     abc_m.add_transition('po', States.PROCESS, States.OUTPUT)
+    abc_m.add_transition('oi', States.OUTPUT, States.INPUT)
+    abc_m.on_enter_INPUT('input')
+    # abc_m.on_enter_PROCESS('ouput')
+    # abc_m.on_enter_INPUT('input')
+    # abc_m.on_enter_PROCESS('ouput')
+    abc_m.on_enter_OUTPUT('output')
 
     print('Done.')
